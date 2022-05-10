085cc03437466608e37f7610de98aa0849945362
Johnny Sørensen <joso@nodes.dk>
2022-05-10 15:23:44 +0200

    added output and cleaned up

--- main.mjs
+++ main.mjs
@@ -3,5 +3,5 @@
 let input = process.env.some_input
 
 console.log(`Our input was ${input}`)
-console.log(`Our input was ${SOME_INPUT}`)
-console.log(`Our input was ${some_input}`)
+
+process.env.SOME_OUTPUT = "success!"
