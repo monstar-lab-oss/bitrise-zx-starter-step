ed901a7a85b27a7cb094cbf86076729e9f81a0e7
Johnny Sørensen <joso@nodes.dk>
2022-05-10 15:22:21 +0200

    some testing

--- main.mjs
+++ main.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env zx
 
-let input = process.env.SOME_INPUT
+let input = process.env.some_input
 
 console.log(`Our input was ${input}`)
+console.log(`Our input was ${SOME_INPUT}`)
+console.log(`Our input was ${some_input}`)
