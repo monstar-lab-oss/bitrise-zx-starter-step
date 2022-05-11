cdefa749bd82f25bef519abf925bd12e465d57c1
Johnny Sørensen <joso@nodes.dk>
2022-05-11 14:44:25 +0200

    less logging

--- step.sh
+++ step.sh
@@ -3,15 +3,21 @@
 # Fail if any command fails
 set -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo -e "Setting up NVM and ZX...\n\n"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash > /dev/null
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+{
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+} > /dev/null
 
-nvm install 16
-nvm use 16
-npm i -g zx
+{
+    nvm use 16 || nvm install 16
+    npm i -g zx
+} > /dev/null
+
+echo -e "\nDone\n\n"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
